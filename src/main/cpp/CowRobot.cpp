4af614ff873f952b013d0c6d91ba8e6ac7fa6d0f
Cindy <cindy.nguyen041214@gmail.com>
2023-10-28 10:50:39 -0700

    added loop to code
    added a for loop to code for testing purposes
    
    Co-Authored-By: Monelle L <114630999+ShrimpHumans@users.noreply.github.com>

--- src/main/cpp/CowRobot.cpp
+++ src/main/cpp/CowRobot.cpp
@@ -35,6 +35,8 @@ CowRobot::CowRobot()
     m_Drivetrain->ResetEncoders();
 
     m_DriveController = new SwerveDriveController(*m_Drivetrain);
+    for (int i = 0; i < 5; i++) 
+  cout << i << "\n";
 }
 
 /**
